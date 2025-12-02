1859137b05ab66d0c3548d20f8c72c2b22dd6d43
Gaurav <gauravpoojary252000@gmail.com>
2025-12-03 00:07:06 +0530

    python-models

--- models/marts/sample.sql
+++ models/marts/sample.sql
@@ -1,1 +1,8 @@
-select * from {{ ref('dim_customers', v=2) }}
+-- Example: Reference dim_customers version 2
+-- select * from {{ ref('dim_customers', v=2) }}
+
+-- Python models are not supported in dbt Fusion
+-- The reference to is_holiday_2025 has been removed since Python models don't work in dbt Fusion
+
+-- Example: Reference an existing model instead
+select * from {{ ref('dim_customers') }} limit 10
