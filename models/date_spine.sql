7a6a363a27c92959e98b3d00f1e36e4ef623d03b
Gaurav <gauravpoojary252000@gmail.com>
2026-02-03 18:40:43 +0530

    dbt ci-cd-5-ii

--- models/date_spine.sql
+++ models/date_spine.sql
@@ -1,6 +1,7 @@
+-- Time spine with daily granularity for MetricFlow / Semantic Layer (DAY or smaller required).
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="cast('2019-01-01' as date)",
-    end_date="cast('2020-01-01' as date)"
+    start_date="cast('2000-01-01' as date)",
+    end_date="cast('2030-12-31' as date)"
    )
 }}
