51db7e77d9ab28bc759c804635d87c566649e3e9
Gaurav <gauravpoojary252000@gmail.com>
2026-02-03 18:20:03 +0530

    dbt ci-cd-3

--- models/legacy/audit.sql
+++ models/legacy/audit.sql
@@ -1,13 +1,18 @@
 {% set old_relation = adapter.get_relation(
       database = target.database,
-      schema = "dbt_gpoojary",
+      schema = target.schema,
       identifier = "customer_orders"
 ) %}
 
 {% set dbt_relation = ref('fct_customer_orders') %}
 
+{% if old_relation is not none %}
 {{ audit_helper.compare_relations(
     a_relation = old_relation,
     b_relation = dbt_relation,
     primary_key = ["order_id"],
 ) }}
+{% else %}
+-- Legacy table customer_orders not present in this target; skip comparison
+select 1 as audit_skipped_legacy_table_not_present
+{% endif %}
