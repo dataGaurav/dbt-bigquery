7fbf05b2fc0bf43a457625693e47626125c5b093
Gaurav <gauravpoojary252000@gmail.com>
2026-02-03 14:58:41 +0530

    versions added

--- models/staging/jaffle_shop/stg_stripe__payments_legacy_v1.sql
+++ models/staging/jaffle_shop/stg_stripe__payments_legacy_v1.sql
@@ -7,9 +7,10 @@ transformed as (
     id as payment_id ,
     orderid as order_id,
     status as payment_status,
-    round(amount/100.0,2) as payment_amount,
+    round(amount/100.0,2) as payment_amount
     from source
 
 )
 
 select * from transformed
+
